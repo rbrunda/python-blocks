0538a56ae68d938dd59be5c220b6769e10579139
Brunda.R <167083396+rbrunda@users.noreply.github.com>
2024-04-20 09:23:59 +0530

    insert at tail in doubly linked list.py

---
+++ insert at tail.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+def printLefttoRight(head):
+    curr = head
+    while curr != None:
+        print(curr.data,end = " ")
+        curr = curr.next
+    print()
+def printRighttoLeft(head):
+    tail = head
+    while tail.next != None:
+        tail = tail.next
+
+        curr = tail
+    while curr != None:
+        print(curr.data,end =" ")
+        curr = curr.prev
+    print()
+def addNodeAtTail(head,val):
+    newBlock = Node(val)
+    if head == None:
+        return newBlock
+    tail = head
+    while tail.next != None:
+        tail = tail.next
+    tail.next = newBlock
+    newBlock.prev = tail
+    return head
+
+n = int(input())
+l = map(int,input().split())
+element = int(input())
+head = None
+for val in l:
+    head = addNodeAtTail(head,val)
+
+printLefttoRight(head)
+printRighttoLeft(head)
+
+head = addNodeAtTail(head,element)
+
+printLefttoRight(head)
+printRighttoLeft(head)
