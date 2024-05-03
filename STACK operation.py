4add223068a00d47c61ff849506c7806b7df02cf
Brunda.R <167083396+rbrunda@users.noreply.github.com>
2024-05-03 10:46:12 +0530

    STACK operation(linked list).py

---
+++ STACK operation.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+      
+      
+def push(top, val):
+    print(val, "inserted")
+    newNode = Node(val)
+    newNode.next = top
+    return newNode
+
+def topValue(top):
+    if top == None:
+        print("Stack is empty")
+        return
+    print(top.data)
+
+def pop(top):
+    if top == None:
+        print("Stack is empty")
+        return None
+    print(top.data)
+    temp = top.next
+    top.next = None
+    return temp
+
+
+def printStack(top):
+    if top == None:
+        print("Stack is empty")
+        return
+  
+    curr = top
+    while curr != None:
+        print(curr.data, end = " ")
+        curr = curr.next
+    print()
+
+def printStackEmpty(top):
+    if top == None:
+        print("Stack is empty")
+    else:
+        print("Stack is not empty")
+
+top = None
+n = int(input().strip())
+while n > 0:
+    word = list(map(int, input().split()))
+    m = word[0]
+    if m == 1:
+        num = word[1]
+        top = push(top, num)
+    elif m == 2:
+        topValue(top)
+    elif m == 3:
+        top = pop(top)
+    elif m == 4:
+        printStack(top)
+    else:
+        printStackEmpty(top)
+    n -= 1
